a1d6b22307c741a4ab961dd5f4ee5a737e15ed41
Connor Linfoot <connorlinfoot@me.com>
2024-05-04 22:00:28 +0100

    Add wrapper and other minor changes

--- src/main/java/dev/linfoot/modapi/HypixelFabricModExample.java
+++ src/main/java/dev/linfoot/modapi/HypixelFabricModExample.java
@@ -1,6 +1,7 @@
 package dev.linfoot.modapi;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -18,11 +19,15 @@ import net.hypixel.modapi.serializer.PacketSerializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.slf4j.Logger;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class HypixelFabricModExample implements ClientModInitializer {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
     private final ServerboundPacketRegistry registry = new ServerboundPacketRegistry();
 
     @Override
@@ -63,17 +68,27 @@ public class HypixelFabricModExample implements ClientModInitializer {
                             if (identifier.equals("direct")) {
                                 sendPacket(new ServerboundLocationPacket());
                             } else {
-                                sendPacket(identifier);
-                                FabricClientCommandSource source = context.getSource();
-                                source.sendFeedback(Text.literal("Sent packet: " + identifier));
+                                if (sendPacket(identifier)) {
+                                    FabricClientCommandSource source = context.getSource();
+                                    source.sendFeedback(Text.literal("Sent packet: " + identifier).withColor(Formatting.GREEN.getColorValue()));
+                                } else {
+                                    FabricClientCommandSource source = context.getSource();
+                                    source.sendError(Text.literal("Failed to send packet: " + identifier));
+                                }
                             }
                             return 1;
                         }))));
     }
 
-    public void sendPacket(String identifier) {
+    public boolean sendPacket(String identifier) {
         HypixelPacket packet = registry.createPacket(identifier);
+        if (packet == null) {
+            LOGGER.warn("Packet {} is not registered", identifier);
+            return false;
+        }
+
         sendPacket(packet);
+        return true;
     }
 
     public void sendPacket(HypixelPacket packet) {
